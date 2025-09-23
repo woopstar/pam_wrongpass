80f0cd467b11da98e14e3f959b1108bf0f4b6e53
Andreas Krüger <andreas@kruger.nu>
2025-09-23 15:52:44 +0200

    Try again

--- pam_wrongpass.go
+++ pam_wrongpass.go
@@ -8,6 +8,19 @@ package main
 #include <security/pam_modules.h>
 #include <stdlib.h>
 
+// Forward declarations to Go (non-standard names)
+int pam_sm_authenticate_go(pam_handle_t *pamh, int flags, int argc, char **argv);
+int pam_sm_acct_mgmt_go(pam_handle_t *pamh, int flags, int argc, char **argv);
+
+// Wrappers with official PAM signatures using const char **argv
+int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    // cast away const only for reading argv values
+    return pam_sm_authenticate_go(pamh, flags, argc, (char**)argv);
+}
+int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    return pam_sm_acct_mgmt_go(pamh, flags, argc, (char**)argv);
+}
+
 static const char* get_pam_user(pam_handle_t *pamh) {
     const char *user = NULL;
     if (pam_get_user(pamh, &user, NULL) != PAM_SUCCESS || user == NULL) {
@@ -33,7 +46,6 @@ const (
 	stateDir        = "/var/lib/pam_wrongpass"
 )
 
-// Resolve path for a given user
 func countFilePathForUser(user string) string {
 	return filepath.Join(stateDir, fmt.Sprintf("%s.count", user))
 }
@@ -60,12 +72,17 @@ func poweroffAsync() {
 	_ = exec.Command("/usr/bin/systemctl", "poweroff").Start()
 }
 
-// Parse maxTries from PAM arguments
 func parseMaxTries(argc C.int, argv **C.char) int {
 	maxTries := defaultMaxTries
+	if argc <= 0 || argv == nil {
+		return maxTries
+	}
+	// read argv
 	argSlice := (*[1 << 16]*C.char)(unsafe.Pointer(argv))[:argc:argc]
-
 	for _, argC := range argSlice {
+		if argC == nil {
+			continue
+		}
 		arg := C.GoString(argC)
 		if strings.HasPrefix(arg, "max_tries=") {
 			valStr := strings.TrimPrefix(arg, "max_tries=")
@@ -77,10 +94,8 @@ func parseMaxTries(argc C.int, argv **C.char) int {
 	return maxTries
 }
 
-// Called during authentication (always called, success or fail)
-//
-//export pam_sm_authenticate
-func pam_sm_authenticate(pamh *C.pam_handle_t, flags C.int, argc C.int, argv **C.char) C.int {
+//export pam_sm_authenticate_go
+func pam_sm_authenticate_go(pamh *C.pam_handle_t, flags C.int, argc C.int, argv **C.char) C.int {
 	userC := C.get_pam_user(pamh)
 	if userC == nil {
 		return C.PAM_SUCCESS
@@ -91,33 +106,27 @@ func pam_sm_authenticate(pamh *C.pam_handle_t, flags C.int, argc C.int, argv **C
 	path := countFilePathForUser(user)
 	cnt := readInt(path)
 	cnt++
-
 	writeInt(path, cnt)
 
 	if cnt >= maxTries {
 		writeInt(path, 0)
 		poweroffAsync()
 	}
-
-	// Do not block login process
+	// never decide auth result
 	return C.PAM_SUCCESS
 }
 
-// Called on account phase after successful authentication
-//
-//export pam_sm_acct_mgmt
-func pam_sm_acct_mgmt(pamh *C.pam_handle_t, flags C.int, argc C.int, argv **C.char) C.int {
+//export pam_sm_acct_mgmt_go
+func pam_sm_acct_mgmt_go(pamh *C.pam_handle_t, flags C.int, argc C.int, argv **C.char) C.int {
 	userC := C.get_pam_user(pamh)
 	if userC == nil {
 		return C.PAM_SUCCESS
 	}
 	user := C.GoString(userC)
-
 	path := countFilePathForUser(user)
 	writeInt(path, 0)
-
 	return C.PAM_SUCCESS
 }
 
-// Required for buildmode=c-shared
+// required for -buildmode=c-shared
 func main() {}
